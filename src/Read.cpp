b09b435cece27577fa317e5b709f1cad511fa20c
Michael Schatz <mschatz@cshl.edu>
2011-09-03 23:12:59 -0400

    guard against out of range quality values

--- src/Read.cpp
+++ src/Read.cpp
@@ -42,19 +42,38 @@ Read::Read(const string & h, const unsigned int* s, const string & q, vector<int
   header = h;
   read_length = rl;
   trim_length = rl;
-  seq = new unsigned int[read_length];
+
+  seq   = new unsigned int[read_length];
   quals = new unsigned int[read_length];
-  prob = new float[read_length];
-  for(int i = 0; i < read_length; i++) {
+  prob  = new float[read_length];
+
+  for(int i = 0; i < read_length; i++) 
+  {
     seq[i] = s[i];
+
     // quality values of 0,1 lead to p < .25
-    quals[i] = q[i] - quality_scale;
-    if(quals[i] >= max_qual) {
-	 cerr << "Quality value " << quals[i] << "larger than maximum allowed quality value " << max_qual << ". Increase the variable 'max_qual' in Read.h." << endl;
-	 exit(EXIT_FAILURE);
+
+    if (q[i] < quality_scale)
+    {
+      cerr << "WARNING: quality value character (" << q[i] << ") below quality scale, assuming 2" << endl;
+      cerr << header << endl;
+      quals[i] = 2;
+    }
+    else
+    {
+      quals[i] = q[i] - quality_scale;
+    }
+
+    if(quals[i] >= max_qual) 
+    {
+	  cerr << "WARNING quality value " << quals[i] << " larger than maximum allowed quality value, reseting to " << max_qual << endl;
+      cerr << header << endl;
+      quals[i] = max_qual;
     }	 
+
     prob[i] = max(.25, 1.0-pow(10.0,-(quals[i]/10.0)));
   }
+
   trusted_read = 0;
   global_like = 1.0;
 }
